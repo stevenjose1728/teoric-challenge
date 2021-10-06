f1d18908ba223a0895fbd86275b1f0bf97f7d5d5
Steven <stevenvarela98@gmail.com>
2021-10-06 10:38:04 -0400

    :pencil: constants to use in tests and static class

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+import { ArkusNexus } from "../models/arkus.model"
+const constants: ArkusNexus = {
+    areas: ['RRHH', 'Development', 'Sales', 'IT Support'],
+    places: ['Tijuana', 'MTY', 'Aguas Calientes'],
+    frontendTechs: ['React', 'React Native', 'VueJS'],
+    backendTechs: ['Python', 'NodeJS']
+}
+export default constants
