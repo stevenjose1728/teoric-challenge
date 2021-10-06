e3918bdd262c165729d695db77f083d6b0bd9102
Steven <stevenvarela98@gmail.com>
2021-10-06 11:29:50 -0400

    :wrench: example of polymorfism by inheritance backendTechs

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,10 +6,22 @@ import {ArkusMty as ArkusMtyModel} from './models/arkusMty.model'
 class ArkusMty extends ArkusNexus{
     address = ''
     employees = 0
+    backendTechs: string[] = []
+    exceptTech = 'Java'
     constructor(props: ArkusNexusModel & ArkusMtyModel){
         super(props)
         this.address = props.address
         this.employees = props.employesCount
+        const i = props.backendTechs.findIndex(element => element === this.exceptTech)
+        let techs = [... props.backendTechs]
+        if(i !== -1){
+            techs.splice(i, 1)
+        }
+        this.backendTechs = techs
+    }
+
+    getBackendTechs(){
+        return 'In this place we work with all backend techs of principal place in Tijuana except: '+this.exceptTech
     }
 
     getEmployees(){
